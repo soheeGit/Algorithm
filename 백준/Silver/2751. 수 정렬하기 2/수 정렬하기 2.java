8519ddbdb6855e5051c98b7555c7abc666817efe
So-hee Jin <63wlsthgml@gmail.com>
2025-09-19 02:43:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2156 ms, Memory: 169908 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
