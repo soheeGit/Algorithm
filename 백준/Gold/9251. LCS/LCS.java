f22e35bc722b996843be5b4e23db1f8093507e22
JinSohee <63wlsthgml@gmail.com>
2025-11-14 14:41:33 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -2,28 +2,31 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static String str1;
-    static String str2;
-    static int time;
-    public static void main(String []args) throws IOException {
+    static String s1, s2;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        str1 = br.readLine();
-        str2 = br.readLine();
-
-        int n = str1.length();
-        int m = str2.length();
-        int dp[][] = new int[n+1][m+1];
-
+        
+        s1 = br.readLine();
+        s2 = br.readLine();
+        
+        int n = s1.length();
+        int m = s2.length();
+        
+        dp = new int[n + 1][m + 1];
+        
+        lcs(n, m);
+        System.out.println(dp[n][m]);
+    }
+    static void lcs(int n, int m) {
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
+                }else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(dp[n][m]);
     }
 }
