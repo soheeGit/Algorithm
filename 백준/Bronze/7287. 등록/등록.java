fe4f59c39c0709437cc83b42ba350db8936b2842
JinSohee <63wlsthgml@gmail.com>
2025-11-15 16:05:55 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(221).append("\n");
+        sb.append("63wlsthgml");
+        
+        System.out.println(sb);
+    }
+}
