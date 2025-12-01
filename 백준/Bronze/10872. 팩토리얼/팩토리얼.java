d0aa92b77ed196647e359332dafc3e8b84cc2269
JinSohee <63wlsthgml@gmail.com>
2025-12-02 01:09:31 +0900

    [Bronze III] Title: 팩토리얼, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int result = 1;
+        for(int i=N; i>0; i--) {
+            result *= i;
+        }
+        
+        System.out.println(result);
+    }
+}
