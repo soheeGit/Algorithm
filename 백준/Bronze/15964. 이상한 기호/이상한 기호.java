c47efa1b0b48d31d9cac08aa7ffbe51dae8c8fdb
JinSohee <63wlsthgml@gmail.com>
2025-12-04 20:36:08 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14244 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        System.out.println((A + B) * (A - B));
+    }
+}
