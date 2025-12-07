7708829ecbab490d17174253fc653dc48ed214a8
JinSohee <63wlsthgml@gmail.com>
2025-12-07 22:27:25 +1000

    [Bronze V] Title: Hello Judge, Time: 88 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
