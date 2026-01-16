7f967ad5c4fa78c1465781d525fbc35dcceaf747
JinSohee <63wlsthgml@gmail.com>
2026-01-16 18:54:12 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 188 ms, Memory: 14964 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
