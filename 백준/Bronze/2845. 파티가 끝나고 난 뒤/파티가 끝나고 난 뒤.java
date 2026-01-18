df4009a3f99a977c5ca58d117cb8825a27b73273
JinSohee <63wlsthgml@gmail.com>
2026-01-18 23:32:27 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int L = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int actual = L * P;
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            int news = Integer.parseInt(st.nextToken());
+            sb.append(news - actual).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
