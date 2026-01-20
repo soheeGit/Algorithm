2db419b86225d08c1236970433ef959f4f89dc55
JinSohee <63wlsthgml@gmail.com>
2026-01-20 14:51:42 +0900

    [Bronze IV] Title: 세수정렬, Time: 132 ms, Memory: 16368 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+    }
+}
