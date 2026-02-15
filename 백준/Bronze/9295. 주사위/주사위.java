bc56c4b3f276747501ddf9c90f3e0c07f09b77b2
JinSohee <63wlsthgml@gmail.com>
2026-02-15 23:06:25 +0900

    [Bronze IV] Title: 주사위, Time: 188 ms, Memory: 16500 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case " + i + ": " + (a + b));
+        }
+    }
+}
