f450944a4f7f231d7d568edcc22729eb2d4cef2c
JinSohee <63wlsthgml@gmail.com>
2026-02-21 00:23:50 +0900

    [Bronze II] Title: 운동, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1173. 운동/운동.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        if (m + T > M) {
+            System.out.println(-1);
+            return;
+        }
+
+        int cur = m;
+        int exercise = 0;
+        int time = 0;
+
+        while (exercise < N) {
+            if (cur + T <= M) {
+                cur += T;
+                exercise++;
+            } else {
+                cur -= R;
+                if (cur < m) {
+                    cur = m;
+                }
+            }
+            time++;
+        }
+        System.out.println(time);
+    }
+}
