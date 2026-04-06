9f938e277c0be711fbf113937ac163d0b63524ba
JinSohee <63wlsthgml@gmail.com>
2026-04-06 23:48:55 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16428 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = N - 1;
+        int count = 0;
+
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+
+            if(sum == M) {
+                count++;
+                left++;
+                right--;
+            } else if(sum < M) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
